6bc045ca1a2220eeabe354bbd0c61b057412030f
ajn19017 <ajn19017@student.mdu.se>
2023-03-02 11:11:11 +0100

    Update RandomForestModel.py

--- Codes/Project/RandomForestModel.py
+++ Codes/Project/RandomForestModel.py
@@ -6,7 +6,7 @@ def rfOptimization(X_train, y_train):
     params = { #Or use param range like:  list(range(1, 10))
         "n_estimators" : [90, 100, 110], #1st iteration: 100 best of [50, 100, 150]
         "criterion" : ["entropy"], #1st iteration: entropy best out of "gini", "entropy", "log_loss"
-        "max_depth" : [19, 20, 21], #1st iteration: 20 best of [10, 20, 30]
+        "max_depth" : [3, 5, 7], #1st iteration: 7 best of [3, 5, 7]
         "min_samples_split" : [1, 2, 3], #1st iteration: 1 best
         "min_samples_leaf" : [1, 2, 3], #1st iteration: 1 best
         "min_weight_fraction_leaf" : [0.0, 0.2, 0.5], #1st iteration: bugged has to be between 0.0 and 0.5
@@ -18,13 +18,13 @@ def rfOptimization(X_train, y_train):
     Best Score 1st: 
     0.9978913394524304
     Best params 1st:
-    {'criterion': 'entropy', 'max_depth': 20, 'min_samples_leaf': 1, 'min_samples_split': 1, 'min_weight_fraction_leaf': 0.0, 'n_estimators': 100}
+    {'criterion': 'entropy', 'max_depth': 7, 'min_samples_leaf': 1, 'min_samples_split': 1, 'min_weight_fraction_leaf': 0.0, 'n_estimators': 100}
 
 
     Best Score 2nd: 
     0.9979068421264552
     Best params 2nd:
-    {'criterion': 'entropy', 'max_depth': 20, 'min_samples_leaf': 1, 'min_samples_split': 1, 'min_weight_fraction_leaf': 0.0, 'n_estimators': 90}
+    {'criterion': 'entropy', 'max_depth': 7, 'min_samples_leaf': 1, 'min_samples_split': 1, 'min_weight_fraction_leaf': 0.0, 'n_estimators': 90}
     """
     optimal_params.fit(X_train, y_train)
 
@@ -35,7 +35,7 @@ def rfOptimization(X_train, y_train):
 
 def prediction(X_train, y_train, X_test):
     #n_estimators is the amount of trees in the forest, criterion determines the optimum mathematical split 
-    classifier = RandomForestClassifier(n_estimators=90, n_jobs=-1, max_depth=20, criterion="entropy")
+    classifier = RandomForestClassifier(n_estimators=90, n_jobs=-1, max_depth=7, criterion="entropy")
 
     classifier.fit(X_train, y_train) 
 
